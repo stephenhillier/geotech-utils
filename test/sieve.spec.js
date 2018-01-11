86a78a895bc17d0c82f53eb2c9e6e46380975c9f
Steve <stephenhillier@gmail.com>
2018-01-10 23:06:36 -0800

    Added unit tests for SieveStack class

--- test/sieve.spec.js
+++ test/sieve.spec.js
@@ -16,8 +16,22 @@ describe('SingleSieve', () => {
 
 describe('SieveStack', () => {
   it('should create a collection of SingleSieve objects if given an array', () => {
-    const testSizes = [20, 10, 2.5, 0.63, 0.08];
+    const testSizes = [20, 10, 0.08];
     const stack = new SieveStack(testSizes);
-    assert.equal(stack[0].size, 20);
+    assert.equal(stack.stack[0].size, 20);
+    assert.equal(stack.stack[1].size, 10);
+    assert.equal(stack.stack[2].size, 0.08);
   });
+  it('should automatically add a pan to the bottom of the stack if not given one', () => {
+    const testSizes = [20, 10, 0.08];
+    const stack = new SieveStack(testSizes);
+    assert.equal(stack.stack[stack.stack.length - 1], 'Pan');
+  });
+  it('should sort out of order inputs', () => {
+    const testSizes = [10, 0.08, 20];
+    const stack = new SieveStack(testSizes);
+    assert.equal(stack.stack[0], 20);
+    assert.equal(stack.stack[1], 10);
+    assert.equal(stack.stack[2], 0.08);
+  })
 });
